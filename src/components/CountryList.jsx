a24778557a2c6757b923fc2a6b9b9ce9a344afff
Ahmed Atef <93956891+aa-ghazzy@users.noreply.github.com>
2025-11-24 04:32:43 +0200

    added only visited countries (no duplicates), cities tab is main and active, clicking on the map brings the form component

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,32 @@
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length) return <Message message="No Countries were added yet" />;
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    } else return arr;
+  }, []);
+
+  // const countries = cities
+  //   .filter(
+  //     (city, index, self) =>
+  //       index === self.findIndex((c) => c.country === city.country)
+  //   )
+  //   .map((city) => ({ country: city.country, emoji: city.emoji }));
+
+  return (
+    <ul className={styles.countryList}>
+      {countries?.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,21 @@
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.map}>Map</div>;
+  const params = useParams();
+  const [searchParam] = useSearchParams();
+  const navigate = useNavigate();
+
+  const lat = searchParam.get("lat");
+  const lng = searchParam.get("lng");
+
+  console.log(params, lat, lng);
+  return (
+    <div className={styles.map} onClick={() => navigate("form")}>
+      Map
+    </div>
+  );
 }
 
 export default Map;
